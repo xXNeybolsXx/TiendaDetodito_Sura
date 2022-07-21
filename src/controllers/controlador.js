d02c7d44ffa708e7b09e95dd6c8f2cd563b94cab
xXNeybolsXx <brandonruda70@gmail.com>
2022-07-21 17:42:57 -0500

    se modifica index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+//CREANDO VARIABLES
+
+let nombreUsuario="James Rodriguez"
+const EDAD_USUARIO=33
+let estaturaUsuario=1.62
+let hinchaDelVerde=true 
+
+console.log(nombreUsuario)
+
+
+console.log(EDAD_USUARIO)
