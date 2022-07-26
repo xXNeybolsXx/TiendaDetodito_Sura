187cc1bfbab920d870251d545fc3e6877564b12c
xXNeybolsXx <brandonruda70@gmail.com>
2022-07-26 17:33:51 -0500

    se crean ejemplos de DOM

---
+++ src/controllers/aprendiendo_DOM.js
@@ -0,0 +1,11 @@
+//Pasos Para Controlar Una Etiqueta Desde JS
+
+//1. Controlando El Contenido
+let etiquetatitulo=document.getElementById("titulo")
+let etiquetaImagen=document.getElementById("foto")
+
+//1.1 Manipulando El Texto De La Etiqueta(Elemento)
+etiquetatitulo.textContent="Jefe Dame Plata"
+
+//1.2 Manipulando El SRC
+etiquetaImagen.src="https://firebasestorage.googleapis.com/v0/b/tiendacomfama-554dc.appspot.com/o/foto11.jpg?alt=media&token=52d8e209-acec-4ee1-9beb-86ada4d79282"
--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,11 +1,59 @@
 //CREANDO VARIABLES
 
-let nombreUsuario="James Rodriguez"
-const EDAD_USUARIO=33
-let estaturaUsuario=1.62
-let hinchaDelVerde=true 
+// let nombreUsuario="James Rodriguez"
+// const EDAD_USUARIO=33
+// let estaturaUsuario=1.62
+// let hinchaDelVerde=true 
 
-console.log(nombreUsuario)
+// console.log(nombreUsuario)
 
 
-console.log(EDAD_USUARIO)
+// console.log(EDAD_USUARIO)
+
+// Mercando Con El Profe
+
+let producto1="Arroz"
+let producto2="Leche"
+let producto3="Huevo"
+let producto4="Carne De Cerdo"
+let producto5="Pollo"
+let producto6="Galletas"
+let producto7="Panela"
+let producto8="Cafe"
+let producto9="Tapa Roja"
+let producto10="Cerveza"
+let producto11="Tomate"
+let producto12="Papas"
+let producto13="Lechuga"
+let producto14="Frijoles"
+let producto15="Aceite"
+
+//ARREGLOS DE DATOS
+//UN ARREGLO ES UNA VARIABLE ESPECIAL QUE ME DEJA ALMACENAR EN MEMORIA MUCHOS DATOS AL MISMO TIPO
+//1.ARREGLO TRADICIONAL
+let numeros=[1,2,8,9,45]
+let nombres=["Juan","Katalina","Mario","Carlos"]
+let contagios=[true,false,false,false,true]
+//1.1 Accediendo A Todos Los Datos(Elementos) de un arreglo
+console.log(numeros)
+//1.2 Accediendo A Un Solo Elemento Del Arreglo
+console.log(numeros[3])
+//2. Arreglos Orientados A Objetos POO o OOP
+let notas=Array(3.5,2.8,3.9,5)
+console.log(notas)
+console.log(notas[1])
+
+//3. Variables Especiales Para Guardar Varios Datos   abre y cierra { } los objetos por dentro tienen atributos
+//De Tipos Diferentes
+//Objetos
+let persona={
+    nombre:"Brandon Ruda",
+    apellidos:["Ruda","Restrpo"],
+    edad: 17,
+    hinchaDelMejor:true
+}
+
+//3.1 Accediendo A Todos Los Datos De Un Objeto
+console.log(persona)
+//3.2 Accediendo A Un Solo Atributo del objeto
+console.log(persona.nombre)
